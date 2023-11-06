91feb411096905325b876ca941abace531d9d0f5
NotToday696 <Maxim46rus6@yandex.ru>
2023-11-06 18:55:02 +0300

    Сука заработай

--- identity_provider/identity_provider/config.py
+++ identity_provider/identity_provider/config.py
@@ -24,7 +24,8 @@ class DBConfig(BaseSettings):
 
 class AuthConfig(BaseSettings):
     secret_key: str = Field(
-        # default="09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7",
+        #default="09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7",
+        #default="y0_AgAAAAARBrROAATuwQAAAADxD5xzQm8lv4ZdTxebLBWSVC7l-sN0m80",
         default="y0_AgAAAAARBrROAATuwQAAAADxD5xzQm8lv4ZdTxebLBWSVC7l-sN0m80",
         allow_mutation=False,
         env='SECRET_KEY',
